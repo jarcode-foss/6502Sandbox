82c78b576a2d8cdf40f6c8ede5778d9ef6ba7f26
0x277F <0x277F@gmail.com>
2015-10-25 18:25:09 -0600

    About half the opcode headers.

--- cpu.c
+++ cpu.c
@@ -1,5 +1,24 @@
 #include "cpu.h"
 
+void init(sandbox_inst *container, cpu_t *cpu){
+    cpu->memory = malloc(MAX_MEM);
+    cpu->reg_A = 0;
+    cpu->reg_X = 0;
+    cpu->reg_Y = 0;
+    cpu.brk = false;
+    cpu.carry = false;
+    cpu.dec = false;
+    cpu.neg = false;
+    cpu->overflow = false;
+    cpu->pc = 0;
+    cpu->zero = false;
+    cpu->irq = false;
+}
+
+void close(cpu_t *cpu){
+    free(cpu->memory);
+}
+
 //Example opcode implementation, what do you think?
 inline void op_lda(cpu_t *cpu){
     int val = cpu->memory[cpu->pc];
--- cpu.h
+++ cpu.h
@@ -27,12 +27,54 @@ typedef struct {
 } cpu_t;
 
 void init(sandbox_inst *container, cpu_t *cpu);
-void close(sandbox_inst *container, cpu_t *cpu);
+void close(cpu_t *cpu);
+
+void inc_pc();
 
 // Opcode Implementations
 // -------------------------------------------------
 //
 
-inline void op_lda(cpu_t *cpu);
+__inline__ void op_lda(cpu_t *cpu);
+__inline__ void op_ldx(cpu_t *cpu);
+__inline__ void op_ldy(cpu_t *cpu);
+__inline__ void op_brk(cpu_t *cpu);
+__inline__ void op_bpl(cpu_t *cpu);
+__inline__ void op_jsr(cpu_t *cpu);
+__inline__ void op_bmi(cpu_t *cpu);
+__inline__ void op_rti(cpu_t *cpu);
+__inline__ void op_bvc(cpu_t *cpu);
+__inline__ void op_rts(cpu_t *cpu);
+__inline__ void op_bvc(cpu_t *cpu);
+__inline__ void op_bcc(cpu_t *cpu);
+__inline__ void op_bcs(cpu_t *cpu);
+__inline__ void op_cpy(cpu_t *cpu);
+__inline__ void op_bne(cpu_t *cpu);
+__inline__ void op_beq(cpu_t *cpu);
+__inline__ void op_ora(cpu_t *cpu);
+__inline__ void op_and(cpu_t *cpu);
+__inline__ void op_eor(cpu_t *cpu);
+__inline__ void op_adc(cpu_t *cpu);
+__inline__ void op_sta(cpu_t *cpu);
+__inline__ void op_cmp(cpu_t *cpu);
+__inline__ void op_sbc(cpu_t *cpu);
+__inline__ void op_cpx(cpu_t *cpu);
+__inline__ void op_bit(cpu_t *cpu);
+__inline__ void op_asl(cpu_t *cpu);
+__inline__ void op_rol(cpu_t *cpu);
+__inline__ void op_lsr(cpu_t *cpu);
+__inline__ void op_ror(cpu_t *cpu);
+__inline__ void op_stx(cpu_t *cpu);
+__inline__ void op_dec(cpu_t *cpu);
+__inline__ void op_inc(cpu_t *cpu);
+__inline__ void op_php(cpu_t *cpu);
+__inline__ void op_clc(cpu_t *cpu);
+__inline__ void op_plp(cpu_t *cpu);
+__inline__ void op_sec(cpu_t *cpu);
+__inline__ void op_pha(cpu_t *cpu);
+__inline__ void op_cli(cpu_t *cpu);
+__inline__ void op_pla(cpu_t *cpu);
+__inline__ void op_sei(cpu_t *cpu);
+__inline__ void op_dey(cpu_t *cpu);
 
 #endif //CPU_H_
